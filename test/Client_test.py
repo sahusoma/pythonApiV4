7a2feac0a6ebfedf9e31a9677309a7b8c41475df
Alejandro Triana <alejandro183@gmail.com>
2020-10-21 18:58:05 -0500

    unit test updated

--- test/Client_test.py
+++ test/Client_test.py
@@ -17,7 +17,7 @@ class ClientTestCase(unittest.TestCase):
         self.test_client.add_preexistence('mock preexistence')
         self.assertEqual(len(self.test_client.get_all_preexistence()), 1)
 
-        self.assertEqual(self.test_client.get_preexistence(0), 'mock preexistence')
+        self.assertEqual(self.test_client.get_preexistence(1), 'mock preexistence')
         self.assertEqual(self.test_client.get_preexistence(1), 'There is no such preexistence')
 
     def test_name_remove_preexistence(self):
@@ -28,10 +28,13 @@ class ClientTestCase(unittest.TestCase):
         self.test_client.add_preexistence('mock preexistence 3')
         self.assertEqual(len(self.test_client.get_all_preexistence()), 3)
         self.test_client.remove_preexistence(1)
-        self.assertEqual(self.test_client.get_preexistence(0), 'mock preexistence')
+        self.assertEqual(self.test_client.get_preexistence(1), 'mock preexistence')
         self.assertEqual(self.test_client.get_preexistence(1), 'mock preexistence 3')
         self.assertEqual(self.test_client.get_preexistence(2), 'There is no such preexistence')
 
 
 if __name__ == '__main__':
     unittest.main()
+    #unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+    # python -m unittest discover -s test -p "*_test.py"
+    # https://stackoverflow.com/questions/11241781/python-unittests-in-jenkins
