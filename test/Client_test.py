1e99ae21a861221c363e3626568f9894b2a3f2b8
Samir Humberto Solano <43558804+sahusoma@users.noreply.github.com>
2020-10-27 08:46:13 -0500

    Update Client_test.py

--- test/Client_test.py
+++ test/Client_test.py
@@ -29,7 +29,7 @@ class ClientTestCase(unittest.TestCase):
         self.assertEqual(len(self.test_client.get_all_preexistence()), 2)
         self.test_client.remove_preexistence(1)
         self.assertEqual(len(self.test_client.get_all_preexistence()), 3)
-        self.assertEqual(self.test_client.get_preexistence(1), 'mock preexistence')
+        self.assertEqual(self.test_client.get_preexistence(0), 'mock preexistence')
         self.assertEqual(self.test_client.get_preexistence(1), 'mock preexistence 3')
         self.assertEqual(self.test_client.get_preexistence(2), 'There is no such preexistence')
 
